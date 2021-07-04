acec9883f05925176d7b4c9157cbd8e122a70af3
DY <Dong.Yang@Student.HTW-Berlin.de>
2021-07-04 17:33:36 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to data Zone
         </a>
       </header>
     </div>
