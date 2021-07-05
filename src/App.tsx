470ce74cdc450e781ffb56056a28a2fd3950246f
DY <Dong.Yang@Student.HTW-Berlin.de>
2021-07-05 12:05:49 +0200

    add heroicons for react

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.css';
+import { HomeIcon, InformationCircleIcon, QuestionMarkCircleIcon, ClipboardListIcon, SearchIcon } from '@heroicons/react/outline'
 
 function App() {
   return (
@@ -27,38 +28,38 @@ function App() {
 
         <nav>
           <a href="#" className="flex items-center space-x-2 py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-            </svg>
+            <HomeIcon className="h-6 w-6" aria-hidden="true" />
             <span>Tweets</span>
           </a>
           <a href="" className="flex items-center space-x-2 py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-            </svg>
+            <ClipboardListIcon className="h-6 w-6" aria-hidden="true" />
             <span>History</span>
           </a>
           <a href="" className="flex items-center space-x-2 py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
+            <InformationCircleIcon className="h-6 w-6" aria-hidden="true" />
             <span>About</span>
           </a>
           <a href="" className="flex items-center space-x-2 py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
+            <QuestionMarkCircleIcon className="h-6 w-6" aria-hidden="true" />
             <span>Help</span>
           </a>
         </nav>
       </div>
 
-      <div className="flex-1 p-10 text-2xl font-bold">
+      <div className="container mx-auto px-4 max-w-screen-xl flex-1 p-10 text-2xl font-bold">
         <header className="bg-white">
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold text-gray-900">
               Tweets
             </h1>
+
+            <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+              <button type="button"
+                className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
+                <span className="sr-only">Search</span>
+                <SearchIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
           </div>
 
           <div className="hidden sm:block" aria-hidden="true">
