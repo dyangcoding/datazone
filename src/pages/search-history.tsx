5efc4658e4994ab31f0555be5ae4aaa25a959c83
DY <Dong.Yang@Student.HTW-Berlin.de>
2021-07-23 12:35:38 +0200

    adjust rule's layout

--- src/pages/search-history.tsx
+++ src/pages/search-history.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { SearchIcon } from "@heroicons/react/outline";
+import { SearchIcon, AdjustmentsIcon } from "@heroicons/react/outline";
 import { useGetRulesQuery } from "../services/api";
 
 const Header: () => JSX.Element = () => {
@@ -34,11 +34,34 @@ const SearchHistory = () => {
         <Fragment>
             <Header />
             {!rules ? <div>No Rules </div> : undefined}
-            <ul>
+            <div className="flex flex-col bg-white divide-y divide-gray-200 my-4">
                 {rules?.map(rule => (
-                    <li key={rule.id}>{rule.keyword}</li>
+                    <div key={rule.id} className="flex justify-between items-center min-w-full px-4 py-4">
+                        <div className="flex">
+                            <div className="flex-shrink-0 h-6 w-6">
+                                <AdjustmentsIcon className="h-6 w-6" aria-hidden="true" />
+                            </div>
+                            <div className="ml-4 text-base font-medium text-gray-900 tracking-wide">
+                                <div className="whitespace-pre-line">
+                                    <span className="py-4">{rule.keyword}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex">
+                            <div className="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <a href="#" className="rounded-md border border-blue-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-blue-700 hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                    Edit
+                                </a>
+                            </div>
+                            <div className="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <a href="#" className="rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                                    Delete
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 ))}
-            </ul>
+            </div>
             {isLoading ? 'Loading' : null}
         </Fragment>
     );
