4640e2f1d4bc9a1c73ae0cb9f98eab610bf769e1
DY <Dong.Yang@Student.HTW-Berlin.de>
2021-08-20 18:15:16 +0200

    add help page

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SearchIcon } from "@heroicons/react/outline";
+import { entries } from "../utils/operatorUtils";
 
 const Header: () => JSX.Element = () => {
     return (
@@ -27,9 +28,51 @@ const Header: () => JSX.Element = () => {
     );
 }
 
+const PageInfo: () => JSX.Element = () => {
+    return (
+        <div className="flex flex-col">
+            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table className="min-w-full divide-y divide-gray-200">
+                            <thead className="bg-gray-50">
+                                <tr>
+                                    <th scope="col"
+                                        className="px-6 py-3 text-left text-xs text-gray-500 uppercase tracking-wider">
+                                        Operator
+                                    </th>
+                                    <th scope="col"
+                                        className="px-6 py-3 text-left text-xs text-gray-500 uppercase tracking-wider">
+                                        Description
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="bg-white divide-y divide-gray-200">
+                                {entries.map((entry, index) => (
+                                    <tr key={index} className="even:bg-gray-50">
+                                        <td className="px-6 py-4 whitespace-nowrap leading-5 font-semibold">
+                                            <span className="px-2 py-1 bg-blue-100 rounded-full text-blue-800">{entry.operator}</span>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-wrap">
+                                            {entry.description}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 const Help: () => JSX.Element = () => {
     return (
-        <Header />
+        <div className="max-w-screen-lg mx-auto">
+            <Header />
+            <PageInfo />
+        </div>
     );
 }
 
